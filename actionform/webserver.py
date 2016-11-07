a2a074d2541915979819f4feb4a2dc74996809c4
Wouter van Atteveldt <wouter@vanatteveldt.com>
2016-11-07 23:33:07 +0100

    Allow custom templating for web server

--- actionform/webserver.py
+++ actionform/webserver.py
@@ -1,5 +1,7 @@
-from jinja2 import Template
+import jinja2
 from flask import request, Flask
+import inspect
+import os
 
 DEFAULT_TEMPLATE = """
 <html>
@@ -20,10 +22,20 @@ DEFAULT_TEMPLATE = """
 </body>
 </html>"""
 
-def run_server(action, template=DEFAULT_TEMPLATE, **kargs):
+def load_template(filename, action=None):
+    path = "./" if action is None else os.path.dirname(inspect.getfile(action))
+    env = jinja2.Environment(loader=jinja2.FileSystemLoader(path or './'))
+    #fn = os.path.join(path, filename); print(fn, os.path.exists(fn))
+    return env.get_template(filename)
 
+def run_server(action, template=None, **kargs):
+    if template is None:
+        template = getattr(action, 'default_template', DEFAULT_TEMPLATE)
     if isinstance(template, str):
-        template = Template(template)
+        if template.endswith(".html"):
+            template = load_template(template, action)
+        else:
+            template = jinja2.Template(template)
     
     app = Flask(action.__name__)
     @app.route('/', methods=['GET'])
@@ -36,8 +48,11 @@ def run_server(action, template=DEFAULT_TEMPLATE, **kargs):
     def post():
         form = action.get_form_class()(request.form)
         a = action(form)
-        result = a.render_result(a.run())
-        title = action.__name__
-        return template.render(**locals())
+        result = a.run()
+        if hasattr(a, 'render_result'):
+            return a.render_result(result, template, locals())
+        else:
+            title = action.__name__
+            return template.render(**locals())
 
     app.run(**kargs)
--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 
 setup(
     name="actionform",
-    version="0.02",
+    version="0.03",
     description="Define an Action as a django form with a run method (with CLI and Web interface)",
     author="Wouter van Atteveldt",
     author_email="wouter@vanatteveldt.com",
